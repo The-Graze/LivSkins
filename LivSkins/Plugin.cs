d125f4f510c5cfb01823ce3e2ec602de2b31c5f3
Graze <me@graze.cc>
2025-02-13 02:17:16 +0000

    Add project files.

---
+++ LivSkins/Plugin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BepInEx;
+using UnityEngine;
+
+namespace LivSkins
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        Plugin()
+        {
+            HarmonyPatches.ApplyHarmonyPatches();
+        }
+    }
+}
---
+++ LivSkins/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace LivSkins
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.graze.gorillatag.livskins";
+        public const string Name = "LivSkins";
+        public const string Version = "1.0.0";
+    }
+}
