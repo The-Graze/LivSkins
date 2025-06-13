572ed48d02841ac0801a7ba8463c092419a16d60
Graze <82724623+The-Graze@users.noreply.github.com>
2025-06-13 18:49:59 +0100

    Update

--- LivSkins/Plugin.cs
+++ LivSkins/Plugin.cs
@@ -18,52 +18,56 @@ namespace LivSkins
         public static ConfigEntry<string> SelectedSkin;
         public static List<Renderer> CamRenderers = new List<Renderer>();
         public static Dictionary<string, Texture> Skins = new Dictionary<string, Texture>();
-        static bool gotSkins;
-        Plugin()
+        private static bool gotSkins;
+
+        private Plugin()
         {
             new Harmony(PluginInfo.GUID).PatchAll(Assembly.GetExecutingAssembly());
         }
 
-        public static void AddRend(Renderer renderer)
+        private void Start()
         {
-            if (renderer.name == "Body")
-            {
-                CamRenderers.Add(renderer);
-            }
-            Plugin.OnSkinChanged(null, null);
-        }
-
-        void Start()
-        {
-            CamModel = Config.Bind("Visuals", "CameraModel", false, "Choose if you would like to use the 'old' Camera model");
+            CamModel = Config.Bind("Visuals", "CameraModel", false,
+                "Choose if you would like to use the 'old' Camera model");
             SelectedSkin = Config.Bind("Visuals", "Skin", "", "The name of the skin selected");
             GorillaTagger.OnPlayerSpawned(delegate
             {
                 foreach (var spawn in Resources.FindObjectsOfTypeAll<LckWallCameraSpawner>())
                 {
-                    Transform RendC = spawn.transform.FindChildRecursive("CameraRenderModel");
-                    if (Plugin.CamModel.Value)
+                    var RendC = spawn.transform.FindChildRecursive("CameraRenderModel");
+                    if (CamModel.Value)
                     {
                         RendC.GetChild(0).gameObject.SetActive(false);
                         RendC.GetChild(2).gameObject.SetActive(true);
-                        Plugin.CamRenderers.Add(RendC.GetChild(2).GetChild(0).GetComponent<Renderer>());
+                        CamRenderers.Add(RendC.GetChild(2).GetChild(0).GetComponent<Renderer>());
                     }
                     else
                     {
-                        Plugin.CamRenderers.Add(RendC.GetChild(0).GetChild(0).GetChild(0).GetComponent<Renderer>());
+                        CamRenderers.Add(RendC.GetChild(0).GetChild(0).GetChild(0).GetComponent<Renderer>());
                     }
                 }
+
                 GetSkinImages();
                 SelectedSkin.SettingChanged += OnSkinChanged;
                 StartCoroutine(DelaySetCosOOpid());
             });
         }
 
+        private void FixedUpdate()
+        {
+            if (!gotSkins) GetSkinImages();
+        }
+
+        public static void AddRend(Renderer renderer)
+        {
+            if (renderer.name == "Body") CamRenderers.Add(renderer);
+            OnSkinChanged(null, null);
+        }
+
         public static void OnSkinChanged(object sender, EventArgs e)
         {
             if (Skins.Keys.Contains(SelectedSkin.Value))
-            {
-                foreach (Renderer r in CamRenderers)
+                foreach (var r in CamRenderers)
                 {
                     var mat = r.material;
                     mat.name = Skins[SelectedSkin.Value].name;
@@ -71,48 +75,32 @@ namespace LivSkins
                     mat.DisableKeyword("_USE_TEX_ARRAY_ATLAS");
                     mat.mainTexture = Skins[SelectedSkin.Value];
                 }
-            }
-
         }
 
-        void FixedUpdate()
-        {
-            if (!gotSkins)
-            {
-                GetSkinImages();
-            }
-        }
-
-        IEnumerator DelaySetCosOOpid()
+        private IEnumerator DelaySetCosOOpid()
         {
             yield return new WaitForSecondsRealtime(3);
-            Plugin.OnSkinChanged(null, null);
+            OnSkinChanged(null, null);
         }
-        static void GetSkinImages()
+
+        private static void GetSkinImages()
         {
-            string modPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            string skinPath = "";
+            var modPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var skinPath = "";
             if (!Directory.Exists(Path.Combine(modPath, "SkinImages")))
-            {
                 skinPath = Directory.CreateDirectory(Path.Combine(modPath, "SkinImages")).Name;
-            }
             else
-            {
                 skinPath = Path.Combine(modPath, "SkinImages");
-            }
 
-            foreach (var skin in Directory.GetFiles(skinPath))
-            {
-                MakeIntoTexture(skin);
-            }
+            foreach (var skin in Directory.GetFiles(skinPath)) MakeIntoTexture(skin);
         }
 
-        static void MakeIntoTexture(string path)
+        private static void MakeIntoTexture(string path)
         {
-            Texture2D tex = new Texture2D(2, 2);
+            var tex = new Texture2D(2, 2);
             var imgdata = File.ReadAllBytes(path);
             tex.LoadImage(imgdata);
-            string name = Path.GetFileNameWithoutExtension(path);
+            var name = Path.GetFileNameWithoutExtension(path);
             tex.name = name;
             tex.filterMode = FilterMode.Point;
             if (!Skins.Keys.Contains(name))
---
+++ LivSkins/SkinHandler.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+using Liv.Lck.GorillaTag;
+using TMPro;
+using UnityEngine;
+
+namespace LivSkins
+{
+    internal class SkinHandler : MonoBehaviour
+    {
+        private TextMeshPro? Lable, SkinName;
+        private GameObject? LeftRightButton, ExtraC;
+
+        private void Start()
+        {
+            ExtraC = new GameObject("Extra Menu");
+            ExtraC.transform.SetParent(transform.FindChildRecursive("Settings Group"));
+            ExtraC.transform.localPosition = new Vector3(-0.07f, -0.11f, 0.1f);
+            ExtraC.transform.localRotation = Quaternion.Euler(315, 0, 0);
+            ExtraC.transform.localScale = new Vector3(0.9f, 0.9f, 0.9f);
+
+            LeftRightButton = Instantiate(transform.FindChildRecursive("FOV").gameObject, ExtraC.transform, false);
+            LeftRightButton.transform.localPosition = Vector3.zero;
+            LeftRightButton.transform.localRotation = Quaternion.Euler(Vector3.zero);
+            LeftRightButton.name = "SkinSelect";
+
+            Lable = LeftRightButton?.transform.FindChildRecursive("Label").GetComponent<TextMeshPro>();
+
+            SkinName = LeftRightButton?.transform.FindChildRecursive("Value").GetComponent<TextMeshPro>();
+
+            if (LeftRightButton != null)
+                foreach (Transform button in LeftRightButton.transform.FindChildRecursive("Triggers").transform)
+                {
+                    if (button.name == "Decrement")
+                    {
+                        button.GetComponent<GtColliderTriggerProcessor>()._onTriggeredEnded.RemoveAllListeners();
+                        button.GetComponent<GtColliderTriggerProcessor>()._onTriggeredStarted.RemoveAllListeners();
+                        button.GetComponent<GtColliderTriggerProcessor>()._onTriggeredStarted.AddListener(Down);
+                    }
+
+                    if (button.name == "Increment")
+                    {
+                        button.GetComponent<GtColliderTriggerProcessor>()._onTriggeredEnded.RemoveAllListeners();
+                        button.GetComponent<GtColliderTriggerProcessor>()._onTriggeredStarted.RemoveAllListeners();
+                        button.GetComponent<GtColliderTriggerProcessor>()._onTriggeredStarted.AddListener(Up);
+                    }
+                }
+        }
+
+        private void Update()
+        {
+            if (SkinName != null)
+            {
+                var toSet = Plugin.SelectedSkin.Value.ToUpper();
+                if (toSet.Count() > 10) toSet = toSet.Substring(0, 6);
+                SkinName.text = toSet;
+            }
+
+            if (Lable != null)
+                if (Lable.text != "SKIN:")
+                    Lable.text = "Skin:";
+        }
+
+        private void Up()
+        {
+            var i = Array.IndexOf(Plugin.Skins.Keys.ToArray(), Plugin.SelectedSkin.Value);
+            if (i >= Plugin.Skins.Keys.Count)
+            {
+            }
+            else
+            {
+                i++;
+                var wawa = Plugin.Skins.Keys.ToList();
+                Plugin.SelectedSkin.SetSerializedValue(wawa[i]);
+            }
+        }
+
+        private void Down()
+        {
+            var i = Array.IndexOf(Plugin.Skins.Keys.ToArray(), Plugin.SelectedSkin.Value);
+            if (i <= 0)
+            {
+            }
+            else
+            {
+                i--;
+                var wawa = Plugin.Skins.Keys.ToList();
+                Plugin.SelectedSkin.SetSerializedValue(wawa[i]);
+            }
+        }
+    }
+}
